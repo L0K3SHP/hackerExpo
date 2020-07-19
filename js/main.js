c71d23268dad29db974f0e4659a58371f028c121
hackerexpo <patiilokesh801@gmail.com>
2020-07-19 13:27:04 +0530

    responsive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const responsive = () => {
     const mainNav = document.querySelector('.navbar')
     const navBarToggle = document.querySelector('.navbar-toggle');
+    const introSec = document.querySelector('.intro');
 
     navBarToggle.addEventListener('click', () =>{
-        mainNav.classList.toggle('active')
+        mainNav.classList.toggle('active');
+        introSec.classList.toggle('intro1');
     });
 
 
